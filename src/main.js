dd51912cb5a5f476efde9a38373620386ee31296
the96 <aar30px2@gmail.com>
2023-04-03 21:54:06 +0900

    eslint対応漏れ

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
-import routes from './route.js'
+import routes from './route'
 import './style.css'
 import App from './App.vue'
-import './util/twitter.js'
+import './util/twitter'
 
 const router = createRouter({ history: createWebHistory(), routes })
 createApp(App).use(router).mount('#app')
