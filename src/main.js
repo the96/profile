29e2a2b342a3c35cb2a865bbb3c879b46c1db703
the96 <aar30px2@gmail.com>
2023-04-03 18:17:45 +0900

    vue-router

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
 import './style.css'
 import App from './App.vue'
+import Index from './components/Index.vue'
 
-createApp(App).mount('#app')
+const routes = [
+  { path: '/', name: 'Home', component: Index },
+]
+
+const router = createRouter({ history: createWebHistory(), routes })
+createApp(App).use(router).mount('#app')
